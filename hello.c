5a4d5b0f52ad9636793ac680cefd1efe0333612b
ejesgarz <54696377+ejesgarz@users.noreply.github.com>
2019-08-30 18:03:47 -0500

    Edited_hello.c
    Personalized hello.c program.

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Emily Jesgarz
+ * No second author - late enrollment
+ * Date: 08/29/19
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Emily Jesgarz and No Second Author\n");
+
+  return 0;
+}
